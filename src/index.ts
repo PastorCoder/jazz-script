8f6ced082b95a250d145afb7f9e039fc857a5c1a
pastorcoder <zuma4good@gmail.com>
2023-08-08 16:17:26 +0100

    annotation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-let age: number = 20;
-if (age > 50) {
-    age += 10;
-};
+let sales: number = 123_456_789;
+let course: string = "Typescript";
+let is_published: boolean = true;
+let level; //type is assumed to be "any"
 
+
+
+
+
+// when initialized annotation can be removed 
+let saler  = 123_456_7890;
+let courses = "Typescripts";
+let is_publishing = true;
+let levels; //type is assumed to be "any"
