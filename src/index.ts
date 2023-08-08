74b004cb56f057922868a496750f68fae08a71f1
pastorcoder <zuma4good@gmail.com>
2023-08-08 16:36:18 +0100

    tuples

--- src/index.ts
+++ src/index.ts
@@ -18,4 +18,17 @@ let number = []; //this is type any
 let numbers: number[] = []; //this is type number[]
 
 numbers.forEach(n => n.toPrecision);
-    console.log(numbers)
+console.log(numbers)
+    
+
+// Tuple 
+let user: [number, string] = [1, "jx"];
+user[0].toLocaleString;
+user[1].matchAll;
+
+user.push(1)
+
+
+// Internally tuples are represented using plain javascript array
+
+//restrict your tutples to only two values
